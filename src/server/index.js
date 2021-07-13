ad4a29ad1163a41ced00f0b9a988a42548d26172
Brian Lovell <brian.lovell@outlook.com>
2021-07-13 14:28:37 -0400

    Add server starter code

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,26 @@
+let path = require('path')
 const express = require('express')
 const app = express()
 const port = 2076
 
 app.listen(port, () => {
     console.log(`Hello there at http://localhost:${port}!`)
+})
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(2076, function () {
+    console.log('Example app listening on port 8080!')
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
 })
