8fd3f31f6fcefcdea40aff1cb29b96e39a5e0e8c
Brian Lovell <brian.lovell@outlook.com>
2021-07-14 13:06:53 -0400

    Copy dev config to prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,9 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path')
 const webpack = require('webpack')
+
+// Plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -17,13 +20,26 @@ module.exports = {
         test: '/\.js$/',
         exclude: /node_modules/,
         loader: "babel-loader"
-      }
+      },
+      {
+        test: /\.scss$/,
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+}
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/client/views/index.html",
       filename: "./index.html"
-    })
+    }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false
+})
   ]
 };
