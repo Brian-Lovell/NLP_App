b45aeb65487e17d50bd71bbaeaad872e675474dc
Brian Lovell <brian.lovell@outlook.com>
2021-07-21 14:39:24 -0400

    Add confidence data

--- src/server/index.js
+++ src/server/index.js
@@ -63,6 +63,7 @@ app.post('/analyze', async function (req, res) {
     apiData.score = data.score_tag
     apiData.agreement = data.agreement
     apiData.subjectivity = data.subjectivity
+    apiData.confidence = data.confidence
     apiData.iron = data.irony
     res.send(apiData)
     console.log(apiData)
